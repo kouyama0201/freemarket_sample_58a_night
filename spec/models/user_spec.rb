7af8d0e2456a6d95945fc30f4943053284f53a26
tiphp452 <tiphp452@gmail.com>
2019-11-25 21:47:39 +0900

    単体テスト(バリデーション)

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is valid with a email" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+    it "is invalid without a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+
+  end
+end
