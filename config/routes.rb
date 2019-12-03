fe1a500b1e93b3b8df3630fe2185d4266c053513
tiphp452 <tiphp452@gmail.com>
2019-12-03 23:39:27 +0900

    ウィザード画面用controller作成
    ・コントローラー作成
    ・ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
+
   root to: 'products#index'
-  # resources :products, only: [:index]
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done'
+    end
+  end
 end
