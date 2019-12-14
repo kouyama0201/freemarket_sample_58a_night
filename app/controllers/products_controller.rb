d454dbe43a875a3926f897f88fddc266b5ee1eec
kenta-m821 <kenta.m821@gmail.com>
2019-12-14 09:50:39 +0900

    コンフリクトの解消、シンボル型への修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,22 @@ class ProductsController < ApplicationController
 
   def new
   end
+
+  def detail
+  end
+
+  def logout
+  end
+
+  def purchase_confirmation
+  end
+
+  def card
+  end
+
+  def card_registration
+  end
+
+  def mypage
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   # resources :products, only: [:index]
-  resources :products
+  resources :signup do
+    collection do
+      get 'registration'
+      get 'phone'
+      get 'address'
+      get 'pay_way'
+      get 'complete'
+    end
+  end
+
+  resources :products do
+    collection do
+      get 'detail'
+      get 'logout'
+      get 'purchase_confirmation'
+      get 'card'
+      get 'card_registration'
+      get 'mypage'
+    end
+  end
 end
