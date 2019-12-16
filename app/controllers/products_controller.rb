e6f3f12f389e5246c903543dd5600f824f60d070
kouyama0201 <kami19900201@gmail.com>
2019-12-16 20:25:39 +0900

    paramsの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,6 +39,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :price, images_attributes: [:id, :image]).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :description, :condition, :delivery_cost, :delivery_origin, :preparatory_days, :price, images_attributes: [:id, :image]).merge(user_id: current_user.id)
   end
 end
