d318e194f6416ad57dc0782b48354416766dfefd
tiphp452 <tiphp452@gmail.com>
2019-12-17 20:44:33 +0900

    routesの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
-    :passwords => 'users/passwords',
-    :omniauth_callbacks =>  "users/omniauth_callbacks"
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   root to: 'products#index'
