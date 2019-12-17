5de9055b093a5d35c970b1ab190e8c6bd3b1c752
kouyama0201 <kami19900201@gmail.com>
2019-12-17 21:41:33 +0900

    販売利益の自動計算機能の実装

---
+++ app/assets/javascripts/profit.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('#price').on('input', function(){
+    var data = $('#price').val();
+    var profit = Math.round(data * 0.9)
+    var fee = (data - profit)
+    if (data >= 300 && data <= 9999999){
+      $('.containt__main__container__inner__sell-form__price__box__center__right').html(fee.toLocaleString())
+      $('.containt__main__container__inner__sell-form__price__box__center__right').prepend('¥')
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').html(profit.toLocaleString())
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').prepend('¥')
+    }
+    else {
+      $('.containt__main__container__inner__sell-form__price__box__center__right').html('-');
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').html('-');
+    }
+  })
+})
