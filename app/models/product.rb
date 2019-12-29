cc4186177e90388d415a5ab7ff32940da2d28be7
kouyama0201 <kami19900201@gmail.com>
2019-12-29 15:34:32 +0900

    enum delivery_costの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
   belongs_to :brand
+
+  enum delivery_cost: {
+    "---":0, 送料込み（出品者負担）:1, 着払い（購入者負担）:2
+  }
 end
