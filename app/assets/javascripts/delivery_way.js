39ef4b7f345203783492c3cc97f85c8d86aa47ad
kouyama0201 <kami19900201@gmail.com>
2019-12-29 15:35:55 +0900

    delivery_wayに関するイベント発火の定義

---
+++ app/assets/javascripts/delivery_way.js
@@ -0,0 +1,50 @@
+$(function(){
+  var array1 = [ '未定','らくらくメルカリ便','ゆうメール','レターパック','普通郵便（定型、定型外）',
+  'クロネコヤマト','ゆうパック','クリックポスト','ゆうパケット' ]
+  var array2 = ['ゆうメール','クロネコヤマト','ゆうパック']
+
+  function appendOption(method){
+    var html = `<option value="${method}">${method}</option>`;
+    return html;
+  }
+
+  $("#delivery_cost").on('change',function(){
+    var delivery_parentCategory = ""
+    delivery_parentCategory = document.getElementById('delivery_cost').value;
+    if (delivery_parentCategory  != "---"){
+    $.ajax({
+        url: 'delivery_way',
+        type: 'GET',
+        data: { parent_name: delivery_parentCategory },
+        dataType: 'json'
+      })
+      .done(function(method) {
+        $('#delivery_method-parent').remove();
+        var methodBoxHtml = '';
+        var insertHTML = '';
+        if (delivery_parentCategory == "送料込み（出品者負担）"){
+          array1.forEach(function(method){
+          insertHTML += appendOption(method);
+          });
+        }else (delivery_parentCategory == "着払い（購入者負担）");{
+            array2.forEach(function(method){
+            insertHTML += appendOption(method);
+            });
+          }
+        methodBoxHtml = `<div class="containt__main__container__inner__sell-form__delivery__box__form-group__added" id="delivery_method-parent"> 
+                          <label>配送の方法</label> 
+                            <span class='containt__main__container__inner__sell-form__form-require'>必須</span> 
+                          <div class='containt__main__container__inner__sell-form__delivery__box__select-wrap'></div>
+                            <select class="containt__main__container__inner__sell-form__delivery__box__select-wrap__list" id="delivery_cost">
+                              <option value="---">---</option>
+                              ${insertHTML}
+                            </select>
+                            <i class='fas fa-chevron-down fa-lg containt__main__container__inner__sell-form__delivery__box__select-wrap__icon'></i>
+                          </div>
+                        </div>`;
+      $('.containt__main__container__inner__sell-form__delivery__box__form-group__cost').append(methodBoxHtml);
+      })
+    }
+    else {$('#delivery_method-parent').remove();}
+  });
+});
