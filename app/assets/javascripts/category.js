0a1d88ea84d0e5f2649a9af2a80948462796dc29
kouyama0201 <kami19900201@gmail.com>
2019-12-30 13:03:06 +0900

    サイズ・ブランドの入力フォームの追加

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -4,6 +4,11 @@ $(function(){
     var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
     return html;
   }
+  // サイズセレクトボックスのオプションを作成
+  function appendSizeOption(size){
+    var html = `<option value="${size.size}">${size.size}</option>`;
+    return html;
+  }
   // 子カテゴリーの表示作成
   function appendChildBox(insertHTML){
     var childSelectHtml = '';
@@ -12,7 +17,7 @@ $(function(){
                           <select class="containt__main__container__inner__sell-form__select-wrap__list" id="child_category" name="category_id">
                             <option value="---" data-category="---">---</option>
                             ${insertHTML}
-                          <select>
+                          </select>
                           <i class='fas fa-chevron-down fa-lg containt__main__container__inner__sell-form__select-wrap__icon'></i>
                         </div>
                       </div>`;
@@ -32,6 +37,36 @@ $(function(){
                             </div>`;
     $('.containt__main__container__inner__sell-form__detail__box__form-group__category').append(grandchildSelectHtml);
   }
+  // サイズ・ブランド入力欄の表示作成
+  function appendSizeBox(insertHTML){
+    var sizeSelectHtml = '';
+    sizeSelectHtml = `<div class="containt__main__container__inner__sell-form__detail__box__form-group__size" id= 'size_wrapper'>
+                        <label>サイズ</label>
+                        <span class='containt__main__container__inner__sell-form__form-require'>必須</span>
+                          <div class='containt__main__container__inner__sell-form__select-wrap'>
+                            <select class="containt__main__container__inner__sell-form__select-wrap__list" id="size" name="size_id">
+                              <option value="---">---</option>
+                              ${insertHTML}
+                            </select>
+                            <i class='fas fa-chevron-down fa-lg containt__main__container__inner__sell-form__select-wrap__icon'></i>
+                          </div>
+                      </div>
+                      <div class="containt__main__container__inner__sell-form__detail__box__form-group__brand" id="brand_wrapper">
+                        <label>ブランド</label>
+                          <span class='containt__main__container__inner__sell-form__form-optional'>任意</span>
+                          <input class="containt__main__container__inner__sell-form__select-wrap__brand" placeholder="例) シャネル" type="text" name="brand">
+                      </div>`;
+    $('.containt__main__container__inner__sell-form__detail__box__form-group__category').append(sizeSelectHtml);
+  }
+  // ブランド入力欄のみの表示作成
+  function appendBrandBox(){
+    brandInputHtml = `<div class="containt__main__container__inner__sell-form__detail__box__form-group__brand" id="brand_wrapper">
+                        <label>ブランド</label>
+                          <span class='containt__main__container__inner__sell-form__form-optional'>任意</span>
+                          <input class="containt__main__container__inner__sell-form__select-wrap__brand" placeholder="例) シャネル" type="text" name="brand">
+                      </div>`;
+    $('.containt__main__container__inner__sell-form__detail__box__form-group__category').append(brandInputHtml);
+  }
   // 親カテゴリー選択後のイベント
   $('#parent_category').on('change', function(){
     var parentCategory = document.getElementById('parent_category').value; //選択された親カテゴリーの名前を取得
@@ -43,7 +78,7 @@ $(function(){
         dataType: 'json'
       })
       .done(function(children){
-        $('#child_wrapper').remove(); //親が変更された時、子以下を削除するする
+        $('#child_wrapper').remove(); //親が変更された時、子以下を削除する
         $('#grandchild_wrapper').remove();
         $('#size_wrapper').remove();
         $('#brand_wrapper').remove();
@@ -56,8 +91,8 @@ $(function(){
       .fail(function(){
         alert('カテゴリー取得に失敗しました');
       })
-    }else{
-      $('#child_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
+    } else {
+      $('#child_wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除する
       $('#grandchild_wrapper').remove();
       $('#size_wrapper').remove();
       $('#brand_wrapper').remove();
@@ -75,7 +110,7 @@ $(function(){
       })
       .done(function(grandchildren){
         if (grandchildren.length != 0) {
-          $('#grandchild_wrapper').remove(); //子が変更された時、孫以下を削除するする
+          $('#grandchild_wrapper').remove(); //子が変更された時、孫以下を削除する
           $('#size_wrapper').remove();
           $('#brand_wrapper').remove();
           var insertHTML = '';
@@ -83,14 +118,46 @@ $(function(){
             insertHTML += appendOption(grandchild);
           });
           appendGrandchildBox(insertHTML);
+        } else {
+          appendBrandBox(); // 紐付くサイズがない場合には、ブランド入力欄のみ生成する
         }
       })
       .fail(function(){
         alert('カテゴリー取得に失敗しました');
       })
-    }else{
+    } else {
       $('#grandchild_wrapper').remove(); //子カテゴリーが初期値になった時、孫以下を削除する
       $('#size_wrapper').remove();
+      $('#brand_wrapper').remove();
+    }
+  });
+  $('.containt__main__container__inner__sell-form__detail__box__form-group__category').on('change', '#grandchild_category', function(){
+    var grandchildId = $('#grandchild_category option:selected').data('category'); //選択された孫カテゴリーのidを取得
+    if (grandchildId != "---"){ //孫カテゴリーが初期値でないことを確認
+      $.ajax({
+        url: 'size',
+        type: 'GET',
+        data: { grandchild_id: grandchildId },
+        dataType: 'json'
+      })
+      .done(function(sizes){
+        $('#size_wrapper').remove(); //孫が変更された時、サイズ欄以下を削除する
+        $('#brand_wrapper').remove();
+        if (sizes.length != 0) {
+        var insertHTML = '';
+          sizes.forEach(function(size){
+            insertHTML += appendSizeOption(size);
+          });
+          appendSizeBox(insertHTML);
+        } else {
+          appendBrandBox(); // 紐付くサイズがない場合には、ブランド入力欄のみ生成する
+        }
+      })
+      .fail(function(){
+        alert('サイズ取得に失敗しました');
+      })
+    } else {
+      $('#size_wrapper').remove(); //孫カテゴリーが初期値になった時、サイズ欄以下を削除する
       $('#brand_wrapper').remove();
     }
   });
