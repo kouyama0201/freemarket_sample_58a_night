6c6a000c4b2601e94148eb2f8a0ddf7bba354d55
kouyama0201 <kami19900201@gmail.com>
2019-12-31 13:40:43 +0900

    アソシエーションの変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,7 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category
-  belongs_to :brand
-  belongs_to :size
+  belongs_to :size, optional: true
 
   enum delivery_cost: {
     "---":0, 送料込み（出品者負担）:1, 着払い（購入者負担）:2
