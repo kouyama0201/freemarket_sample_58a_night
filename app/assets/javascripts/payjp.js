df97e5e0a12112fdcbf5e30229c2a4fa6bf8d393
tiphp452 <tiphp452@gmail.com>
2020-01-02 20:51:05 +0900

    Update payjp.js

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,8 +1,9 @@
-const PAYJP_KEY = gon.payjp_key;
+const PAYJP_KEY = gon.payjp_key; // 環境変数の定義
 
-document.addEventListener(
+// pay_wayのページで入力したカード情報を使用してPAY.JPからトークン取得
+document.addEventListener( 
   "DOMContentLoaded", e => {
-    if (document.getElementById("token_submit") != null) { 
+    if (document.getElementById("token_submit") != null) {
       Payjp.setPublicKey(PAYJP_KEY);
       let btn = document.getElementById("token_submit"); 
       btn.addEventListener("click", e => { 
@@ -20,8 +21,8 @@ document.addEventListener(
             $("#exp_month").removeAttr("name");
             $("#exp_year").removeAttr("name");
             $("#card_token").append(
-            $('<input type="hidden" name="payjp-token">').val(response.id)
-            ); 
+              $('<input type="hidden" name="payjp-token">').val(response.id)
+            );
             document.inputForm.submit()
           } else {
             alert("カード情報が正しくありません。");
