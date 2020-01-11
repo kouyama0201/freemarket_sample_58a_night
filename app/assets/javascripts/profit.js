7fbe0059461df87bdc937e4fad6e093285c3d983
tiphp452 <tiphp452@gmail.com>
2020-01-11 17:51:05 +0900

    イベント発火基準を修正

--- app/assets/javascripts/profit.js
+++ app/assets/javascripts/profit.js
@@ -1,6 +1,6 @@
 $(function(){
-  $('containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
-    var data = $('#price').val();
+  $('.containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
+    var data = $('.containt__main__container__inner__sell-form__price__box__top__right__input').val();
     var profit = Math.round(data * 0.9)
     var fee = (data - profit)
     if (data >= 300 && data <= 9999999){
