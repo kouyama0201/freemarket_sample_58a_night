1f11adc1a1f22e4c61f494bde081cb581fceb7f9
tiphp452 <tiphp452@gmail.com>
2020-01-12 18:40:58 +0900

    不要なデバッグ用の記述削除

--- app/assets/javascripts/profit.js
+++ app/assets/javascripts/profit.js
@@ -1,14 +1,8 @@
-
-
 $(function() { // 販売利益の計算
-  alert("profit ok");
   $('.containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
     var data = $('.containt__main__container__inner__sell-form__price__box__top__right__input').val();
-    console.log(data);
     var profit = Math.round(data * 0.9);
-    console.log(profit);
     var fee = (data - profit);
-    console.log(fee);
     if (data >= 300 && data <= 9999999) {
       $('.containt__main__container__inner__sell-form__price__box__center__right').html(fee.toLocaleString());
       $('.containt__main__container__inner__sell-form__price__box__center__right').prepend('¥');
