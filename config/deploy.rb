041f62dc8c4d577820e5a820a0d35c62cdce2a1a
tiphp452 <tiphp452@gmail.com>
2020-01-12 09:15:16 +0900

    環境変数設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :keep_releases, 5
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  DEVISE_SECRET_KEY: ENV["DEVISE_SECRET_KEY"],
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
