2e0c3c8841ce9b663c82acc88619f759e4800fe8
tiphp452 <tiphp452@gmail.com>
2020-01-12 16:30:25 +0900

    JSファイルの集約

--- app/assets/javascripts/jquery.validate.handler.user.js
+++ app/assets/javascripts/jquery.validate.handler.user.js
@@ -232,3 +232,20 @@ $(function () {
     $(this).valid();
   });
 });
+
+$(function () {
+  $('.containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
+    var data = $('.containt__main__container__inner__sell-form__price__box__top__right__input').val();
+    var profit = Math.round(data * 0.9);
+    var fee = (data - profit);
+    if (data >= 300 && data <= 9999999) {
+      $('.containt__main__container__inner__sell-form__price__box__center__right').html(fee.toLocaleString());
+      $('.containt__main__container__inner__sell-form__price__box__center__right').prepend('¥');
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').html(profit.toLocaleString());
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').prepend('¥');
+    } else {
+      $('.containt__main__container__inner__sell-form__price__box__center__right').html('-');
+      $('.containt__main__container__inner__sell-form__price__box__bottom__right').html('-');
+    }
+  })
+})
