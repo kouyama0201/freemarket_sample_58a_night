5c9b6f42a1f1ea2ffe4cd2964b4bd75f85d33aae
tiphp452 <tiphp452@gmail.com>
2020-01-12 17:28:55 +0900

    ファイルの一時修正

--- app/assets/javascripts/productNew.js
+++ app/assets/javascripts/productNew.js
@@ -2,7 +2,7 @@
 
 $(function() { // 販売利益の計算
   alert("profit ok");
-  $('.containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
+  $('#caliculate').on('input', function () {
     var data = $('.containt__main__container__inner__sell-form__price__box__top__right__input').val();
     console.log(data);
     var profit = Math.round(data * 0.9);
