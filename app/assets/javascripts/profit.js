96e86698cab4cb92b0d00b0d6196fee0f60275d9
kouyama0201 <kami19900201@gmail.com>
2020-01-15 09:42:19 +0900

    小数点以下を切り捨てする記述に変更

--- app/assets/javascripts/profit.js
+++ app/assets/javascripts/profit.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.containt__main__container__inner__sell-form__price__box__top__right__input').on('input', function () {
     var data = $('.containt__main__container__inner__sell-form__price__box__top__right__input').val();
-    var profit = Math.round(data * 0.9)
+    var profit = Math.floor(data * 0.9)
     var fee = (data - profit)
     if (data >= 300 && data <= 9999999){
       $('.containt__main__container__inner__sell-form__price__box__center__right').html(fee.toLocaleString())
