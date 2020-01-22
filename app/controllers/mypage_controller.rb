3b8e58826bd9fe01ef09ba5f64d712577eaf0255
kouyama0201 <kami19900201@gmail.com>
2020-01-22 19:50:02 +0900

    出品ページのインスタンスを設定

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,6 +2,15 @@ class MypageController < ApplicationController
   def show # マイページ
   end
 
+  def exhibiting
+    @user = User.find(params[:id])
+    @product = @user.products.page(params[:page]).per(12).order("created_at DESC")
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   def card # 支払い方法
   end
 
