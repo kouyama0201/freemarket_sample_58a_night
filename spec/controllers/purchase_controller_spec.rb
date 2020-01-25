152a89b8ad05c2f6844c11ad1a46473d0278640c
tiphp452 <tiphp452@gmail.com>
2020-01-25 23:08:31 +0900

    テストコード修正

--- spec/controllers/purchase_controller_spec.rb
+++ spec/controllers/purchase_controller_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 describe PurchaseController, type: :controller do
   let(:user) { create(:user) }
-  let(:card) { create(:card) }
+  let(:card) { create(:card, user_id: user.id) }
   describe 'GET #show' do
       context 'ログイン時' do
       before do
         login user
       end
       it "@productは正しくアサインされるか" do
-        card = create(:card, user_id: user.id)
         allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
         product = create(:product)
-        get :show, params: { id: product }
+        get :show, params: { id: product, card: card}
         expect(assigns(:product)).to eq product
       end
       it "showアクションのページに遷移するか" do
-        card = create(:card, user_id: user.id)
         allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_customer_information)
         product = create(:product)
-        get :show, params: { id: product }
+        get :show, params: { id: product,card: card}
         expect(response).to render_template :show
       end
     end
@@ -30,17 +28,15 @@ describe PurchaseController, type: :controller do
         login user
       end
       it "@productは正しくアサインされるか" do
-        card = create(:card, user_id: user.id)
         allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)        
         product = create(:product)
-        get :pay, params: { id: product }
+        get :pay, params: { id: product, card: card }
         expect(assigns(:product)).to eq product
       end
       it "購入が完了しトップページに遷移するか" do
-        card = create(:card, user_id: user.id)
         allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)        
         product = create(:product)
-        get :pay, params: { id: product }
+        get :pay, params: { id: product, card: card }
         expect(response).to redirect_to root_path
       end
     end
