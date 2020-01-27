81bc69c74ec8498ef3f44af798cbd9c495a57542
kouyama0201 <kami19900201@gmail.com>
2020-01-27 14:04:50 +0900

    detail_searchとransackのメソッドを定義

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,18 @@
 class SearchesController < ApplicationController
+  before_action :set_ransack
+
   def index
     @products = Product.search(params[:search]).where.not(transaction_status: 2).page(params[:page]).per(20).order("created_at DESC")
     @search = params[:search]
+  end
+
+  def detail_search
+    @search_product = Product.ransack(params[:q]) 
+    @products = @search_product.result.page(params[:page]).per(20).order("created_at DESC")
+  end
+
+  private
+  def set_ransack
+    @q = Product.ransack(params[:q])
   end
 end
